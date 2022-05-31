e4756017631636c163f83e6fdc2f2fa8c6fca83d
João Lucas De Souza Paz <joaolucasdesouzapaz1@gmail.com>
2022-05-31 14:38:22 -0300

    CRUD 31/05/2022

--- BlogPessoal/src/main/java/com/blogpessoal/BlogPessoal/model/Postagem.java
+++ BlogPessoal/src/main/java/com/blogpessoal/BlogPessoal/model/Postagem.java
@@ -6,12 +6,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import net.bytebuddy.utility.nullability.NeverNull;
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //indica para o string que o objeto criado se torna uma tabela
 @Entity
@@ -19,56 +19,69 @@ import net.bytebuddy.utility.nullability.NeverNull;
 //indica o nome dessa tabela
 @Table(name = "postagem")
 public class Postagem {
-	
-	//indica que o id será uma chave primaria
+
+	// indica que o id será uma chave primaria
 	@Id
-	
-	//define o autoIncremente para o id e previnir que não vai existe nenhum id igual
+
+	// define o autoIncremente para o id e previnir que não vai existe nenhum id
+	// igual
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 500)
 	private String titulo;
-	
-	
+
 	private String texto;
-	
-	
+
 	private Date data = new java.sql.Date(System.currentTimeMillis());
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 	
 	
 	
 	
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
 	};
 
-	
-	
-	
-	
 }
